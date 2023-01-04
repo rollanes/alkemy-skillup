49693b9138444bf1d44ae54bd8e880abd72d2269
Rocio Llanes <rollanes13@gmail.com>
2023-01-04 02:51:16 -0300

    se agrega headless ui para cambios en navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,11 @@
+import { useState, useRef } from "react";
 import { Link } from "react-router-dom";
+import { Transition } from "@headlessui/react";
 
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const ref = useRef();
+
   return (
     <header aria-label="Site Header" className="bg-white">
       <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
@@ -63,7 +68,14 @@ export const Header = () => {
               </div>
 
               <div className="block md:hidden">
-                <button className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75">
+                <button
+                  onClick={() => setIsOpen(!isOpen)}
+                  type="button"
+                  className="rounded bg-gray-100 p-2 text-gray-600 transition hover:text-gray-600/75"
+                  aria-controls="mobile-menu"
+                  aria-expanded="false"
+                >
+                  {!isOpen ? (
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     className="h-5 w-5"
@@ -78,12 +90,63 @@ export const Header = () => {
                       d="M4 6h16M4 12h16M4 18h16"
                     />
                   </svg>
+                  ) : (
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                  )}
                 </button>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <Transition
+          show={isOpen}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+            <div className="md:hidden" id="mobile-menu">
+              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                <a
+                  href="#"
+                  className="text-gray-500 hover:bg-teal-500 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Dashboard
+                </a>
+
+                <a
+                  href="#"
+                  className="text-gray-500 hover:bg-teal-500 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Team
+                </a>
+
+                <a
+                  href="#"
+                  className="text-gray-500 hover:bg-teal-500 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Projects
+                </a>
+
+              </div>
+            </div>
+        </Transition>
     </header>
   );
 };
