61af82d6d7996d6295b2aee2e3c74cc57345eb61
Rocio Llanes <rollanes13@gmail.com>
2023-01-04 15:45:35 -0300

    se renderizan los datos en los componentes

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,32 +1,27 @@
 import { Link } from "react-router-dom";
 
-export const MovieItem = () => {
+export const MovieItem = ({ title, description, image, rating }) => {
   return (
-    <div className="grid gap-4 mt-8 sm:grid-cols-2 lg:grid-cols-4">
-      <Link href="#" className="block group">
-        <img
-          src="https://images.unsplash.com/photo-1592921870789-04563d55041c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-          alt=""
-          className="h-[250px] w-full object-cover sm:h-[350px]"
-        />
+    <Link to="/" className="block group">
+      <img
+        src={ `https://image.tmdb.org/t/p/w500/${ image }` }
+        alt={ title }
+        className="h-[250px] w-full object-cover sm:h-[350px]"
+      />
 
-        <div className="flex justify-between mt-3 text-sm">
-          <div>
-            <h3 className="text-gray-900 group-hover:underline group-hover:underline-offset-4">
-              Movie title
-            </h3>
+      <div className="flex justify-between mt-3 text-sm">
+        <div>
+          <h3 className="text-gray-900 group-hover:underline group-hover:underline-offset-4">
+            { title }
+          </h3>
 
-            <p className="mt-1.5 max-w-[45ch] text-xs text-gray-500">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Voluptatibus, accusamus dicta, neque sequi architecto nemo, quasi
-              numquam voluptatem ipsam veritatis expedita ut tempora distinctio
-              hic ratione possimus. Odio, ipsa molestiae.
-            </p>
-          </div>
-
-          <p className="text-gray-900">Rating</p>
+          <p className="mt-1.5 max-w-[45ch] text-xs text-gray-500">
+            { description.substring(0,150) }...
+          </p>
         </div>
-      </Link>
-    </div>
+
+        <p className="text-gray-900">Rated: { rating }</p>
+      </div>
+    </Link>
   );
 };
--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -5,18 +5,16 @@ import { Navigate } from "react-router-dom";
 import { MovieItem } from "./MovieItem";
 
 export const MoviesList = () => {
+  const [moviesList, setMoviesList] = useState([]);
 
-  const [ moviesList, setMoviesList ] = useState([]);
-
-  let token = localStorage.getItem('token');
+  let token = localStorage.getItem("token");
   useEffect(() => {
-    const endPoint = 'https://api.themoviedb.org/3/discover/movie?api_key=a3afb72cd27c498d0a4ceccdbb854548&page=1';
-    axios
-      .get(endPoint)
-      .then(res => {
-        const apiData = res.data;
-        setMoviesList(apiData.results);
-      })
+    const endPoint =
+      "https://api.themoviedb.org/3/discover/movie?api_key=a3afb72cd27c498d0a4ceccdbb854548&page=1";
+    axios.get(endPoint).then((res) => {
+      const apiData = res.data;
+      setMoviesList(apiData.results);
+    });
   }, [setMoviesList]);
 
   return (
@@ -24,18 +22,19 @@ export const MoviesList = () => {
       {!token ? (
         <Navigate to="/" />
       ) : (
-      <div className="max-w-screen-xl px-4 py-4 mx-auto sm:px-6 sm:py-6 lg:px-8 mb-20">
-        <header>
-          <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
-            Movies Collection
-          </h2>
-        </header>
-
-        <MovieItem />
-
-      </div>
+        <div className="max-w-screen-xl px-4 py-4 mx-auto sm:px-6 sm:py-6 lg:px-8 mb-20">
+          <header>
+            <h2 className="text-xl font-bold text-gray-900 sm:text-3xl">
+              Movies Collection
+            </h2>
+          </header>
+          <div className="grid gap-4 mt-8 sm:grid-cols-2 lg:grid-cols-4">
+            {moviesList.map((movie, index) => {
+              return <MovieItem title={movie.original_title} description={movie.overview} image={movie.poster_path} rating={movie.vote_average} key={index} />;
+            })}
+          </div>
+        </div>
       )}
-
     </>
   );
 };
