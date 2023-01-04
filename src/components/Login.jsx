217a6ef9e4956992bfc192f888a297965702ade5
Rocio Llanes <rollanes13@gmail.com>
2023-01-03 23:40:57 -0300

    se agrega validacion de formulario de login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,22 @@
 export const Login = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    const regexEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    if (email === "" || password === "") {
+    }
+
+    if (email !== "" && regexEmail.test(email)) {
+    }
+
+    if(email === 'challenge@alkemy.org' || password !== 'react'){
+      
+    }
+  };
+
   return (
     <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
       <div className="mx-auto max-w-lg text-center">
@@ -7,9 +25,12 @@ export const Login = () => {
         </h1>
       </div>
 
-      <form action="" className="mx-auto mt-8 mb-0 max-w-md space-y-4">
+      <form
+        onSubmit={handleSubmit}
+        className="mx-auto mt-8 mb-0 max-w-md space-y-4"
+      >
         <div>
-          <label for="email" className="sr-only">
+          <label htmlFor="email" className="sr-only">
             Email
           </label>
 
@@ -18,12 +39,13 @@ export const Login = () => {
               type="email"
               className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
               placeholder="Enter email"
+              name="email"
             />
           </div>
         </div>
 
         <div>
-          <label for="password" className="sr-only">
+          <label htmlFor="password" className="sr-only">
             Password
           </label>
           <div className="relative">
@@ -31,6 +53,7 @@ export const Login = () => {
               type="password"
               className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
               placeholder="Enter password"
+              name="password"
             />
           </div>
         </div>
@@ -40,7 +63,7 @@ export const Login = () => {
             type="submit"
             className="ml-3 inline-block rounded-lg bg-blue-500 px-5 py-3 text-sm font-medium text-white"
           >
-            Sign in
+            Log in
           </button>
         </div>
       </form>
