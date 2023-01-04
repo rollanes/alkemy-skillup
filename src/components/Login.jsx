e2b45a1fd0cfa743b23ebc590db46b7f9028aa13
Rocio Llanes <rollanes13@gmail.com>
2023-01-04 13:29:59 -0300

    se agrega proteccion de rutas

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,42 +1,43 @@
 import axios from "axios";
 import Swal from "sweetalert2";
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 export const Login = () => {
 
   const navigate = useNavigate();
-
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
+
 
     const regexEmail =
       /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
     if (email === "" || password === "") {
       Swal.fire({
-        title: 'Oops...',
-        text: 'Fields cannot be empty!',
-        icon: 'error'
+        title: "Oops...",
+        text: "Fields cannot be empty!",
+        icon: "error",
       });
       return;
     }
 
     if (email !== "" && !regexEmail.test(email)) {
       Swal.fire({
-        title: 'Oops...',
-        text: 'You should enter a valid email!',
-        icon: 'error'
+        title: "Oops...",
+        text: "You should enter a valid email!",
+        icon: "error",
       });
       return;
     }
 
     if (email !== "challenge@alkemy.org" || password !== "react") {
       Swal.fire({
-        title: 'Error',
+        title: "Error",
         text: "Invalid credentials",
-        icon: 'error'
+        icon: "error",
       });
     }
 
@@ -44,69 +45,75 @@ export const Login = () => {
       .post("http://challenge-react.alkemy.org", { email, password })
       .then((res) => {
         Swal.fire({
-          title: 'Nice!',
+          title: "Nice!",
           text: "You're in",
-          icon: 'success'
+          icon: "success",
         });
 
         const receivedToken = res.data.token;
-        localStorage.setItem('token', receivedToken);
-        navigate('/movies');
+        localStorage.setItem("token", receivedToken);
+        navigate("/movies");
       });
   };
 
+  let token = localStorage.getItem('token');
+
   return (
-    <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-      <div className="mx-auto max-w-lg text-center">
-        <h1 className="text-2xl font-bold sm:text-3xl">
-          Login to see our content!
-        </h1>
+    <>
+      { token && <Navigate replace={true} to="/movies" /> }
+
+      <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+        <div className="mx-auto max-w-lg text-center">
+          <h1 className="text-2xl font-bold sm:text-3xl">
+            Login to see our content!
+          </h1>
+        </div>
+
+        <form
+          onSubmit={handleSubmit}
+          className="mx-auto mt-8 mb-0 max-w-md space-y-4"
+        >
+          <div>
+            <label htmlFor="email" className="sr-only">
+              Email
+            </label>
+
+            <div className="relative">
+              <input
+                type="email"
+                className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
+                placeholder="Enter email"
+                name="email"
+                autoComplete="email"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="password" className="sr-only">
+              Password
+            </label>
+            <div className="relative">
+              <input
+                type="password"
+                className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
+                placeholder="Enter password"
+                name="password"
+                autoComplete="password"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-center">
+            <button
+              type="submit"
+              className="ml-3 inline-block rounded-lg bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
+            >
+              Log in
+            </button>
+          </div>
+        </form>
       </div>
-
-      <form
-        onSubmit={handleSubmit}
-        className="mx-auto mt-8 mb-0 max-w-md space-y-4"
-      >
-        <div>
-          <label htmlFor="email" className="sr-only">
-            Email
-          </label>
-
-          <div className="relative">
-            <input
-              type="email"
-              className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
-              placeholder="Enter email"
-              name="email"
-              autoComplete="email"
-            />
-          </div>
-        </div>
-
-        <div>
-          <label htmlFor="password" className="sr-only">
-            Password
-          </label>
-          <div className="relative">
-            <input
-              type="password"
-              className="w-full rounded-lg border-gray-200 p-4 pr-12 text-sm shadow-sm"
-              placeholder="Enter password"
-              name="password"
-              autoComplete="password"
-            />
-          </div>
-        </div>
-
-        <div className="flex items-center justify-center">
-          <button
-            type="submit"
-            className="ml-3 inline-block rounded-lg bg-teal-600 px-5 py-2.5 text-sm font-medium text-white shadow"
-          >
-            Log in
-          </button>
-        </div>
-      </form>
-    </div>
+    </>
   );
 };
